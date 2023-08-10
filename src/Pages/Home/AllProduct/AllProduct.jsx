a8b519ae3db16d0187c7715d3e561dee8916cd1f
dev-talukdar <dev.talukdaar@gmail.com>
2023-08-10 18:39:01 +0600

    tab is now configuring

--- src/Pages/Home/AllProduct/AllProduct.jsx
+++ src/Pages/Home/AllProduct/AllProduct.jsx
@@ -1,11 +1,52 @@
- 
+
 
 const AllProduct = () => {
     return (
-        <div>
-            
+        <div className="tabs">
+            <a className="tab tab-lifted">Tab 1</a>
+            <a className="tab tab-lifted tab-active">Tab 2</a>
+            <a className="tab tab-lifted">Tab 3</a>
         </div>
+
     );
 };
 
 export default AllProduct;
+
+{/* <div className="container mx-auto mt-6">
+            <div className="flex bg-teal-100 rounded-md shadow-md">
+                <label className="flex-grow p-2 text-gray-700 bg-white rounded-l-md transition  cursor-pointer hover:bg-cyan-600 focus:bg-cyan-600 focus:text-white focus:outline-none hover:text-white hover:ease-in duration-500 ...">
+                    <input type="radio" name="radio" className="hidden" />
+                    <span className="text-sm uppercase text-center items-center justify-center flex font-medium">Dates</span>
+                </label>
+
+                <label className="flex-grow p-2 text-gray-700 bg-white transition  cursor-pointer hover:bg-cyan-600 focus:bg-cyan-600 focus:text-white focus:outline-none hover:text-white hover:ease-in duration-500 ...">
+                    <input type="radio" name="radio" className="hidden" />
+                    <span className="text-sm uppercase text-center items-center justify-center flex font-medium">Peanuts</span>
+                </label>
+
+                <label className="flex-grow p-2 text-gray-700 bg-white rounded-r-md transition  cursor-pointer hover:bg-cyan-600 focus:bg-cyan-600 focus:text-white focus:outline-none hover:text-white hover:ease-in duration-500 ...">
+                    <input type="radio" name="radio" className="hidden" />
+                    <span className="text-sm uppercase  items-center justify-center flex text-center font-medium">Seeds</span>
+                </label>
+
+                <label className="flex-grow p-2 text-gray-700 bg-white rounded-r-md transition  cursor-pointer hover:bg-cyan-600 focus:bg-cyan-600 focus:text-white focus:outline-none hover:text-white hover:ease-in duration-500 ...">
+                    <input type="radio" name="radio" className="hidden" />
+                    <span className="text-sm uppercase  items-center justify-center flex text-center font-medium">Honey</span>
+                </label>
+
+                <label className="flex-grow p-2 text-gray-700 bg-white rounded-r-md transition  cursor-pointer hover:bg-cyan-600 focus:bg-cyan-600 focus:text-white focus:outline-none hover:text-white hover:ease-in duration-500 ...">
+                    <input type="radio" name="radio" className="hidden" />
+                    <span className="text-sm uppercase  items-center justify-center flex text-center font-medium">Oils</span>
+                </label>
+
+                <label className="flex-grow p-2 text-gray-700 bg-white rounded-r-md transition  cursor-pointer hover:bg-cyan-600 focus:bg-cyan-600 focus:text-white focus:outline-none hover:text-white  hover:ease-in duration-500 ...">
+                    <input type="radio" name="radio" className="hidden" />
+                    <span className="text-sm uppercase  items-center justify-center flex text-center font-medium">Spice & Herbs</span>
+                </label>
+                
+            </div>
+        </div> */}
+
+
+
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -16,6 +16,7 @@ const Home = () => {
 
              <Banner></Banner>
              <ProductListing></ProductListing>
+             
              <AllProduct></AllProduct>
              <Category></Category> 
              <PopularProduct></PopularProduct>
