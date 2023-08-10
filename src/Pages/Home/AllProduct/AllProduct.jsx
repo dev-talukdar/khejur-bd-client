dc3ae211ec9899f9dc81518b62800eaa45ed1d28
dev-talukdar <dev.talukdaar@gmail.com>
2023-08-10 21:29:06 +0600

    tab  are built now

--- src/Pages/Home/AllProduct/AllProduct.jsx
+++ src/Pages/Home/AllProduct/AllProduct.jsx
@@ -1,52 +1,35 @@
-
+import { useState } from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 const AllProduct = () => {
+    const [tabIndex, setTabIndex] = useState(0)
     return (
-        <div className="tabs">
-            <a className="tab tab-lifted">Tab 1</a>
-            <a className="tab tab-lifted tab-active">Tab 2</a>
-            <a className="tab tab-lifted">Tab 3</a>
-        </div>
+        <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+            <TabList className='container mx-auto mt-6'>
+                <div className="flex rounded-md shadow-md">
+                <Tab className="flex-grow p-2 text-gray-700 bg-white transition  cursor-pointer uppercase font-medium flex justify-center hover:bg-yellow-600 focus:bg-yellow-600 focus:text-white focus:outline-none hover:text-white hover:ease-in duration-500 ...">Dates</Tab>
+                <Tab className="flex-grow p-2 text-gray-700 bg-white  border-l-2  transition  cursor-pointer uppercase font-medium flex justify-center hover:bg-yellow-600 focus:bg-yellow-600 focus:text-white focus:outline-none hover:text-white hover:ease-in duration-500 ...">Peanuts</Tab>
+                <Tab className="flex-grow p-2 text-gray-700 bg-white  border-l-2  transition  cursor-pointer uppercase font-medium flex justify-center hover:bg-yellow-600 focus:bg-yellow-600 focus:text-white focus:outline-none hover:text-white hover:ease-in duration-500 ...">Seeds</Tab>
+                <Tab className="flex-grow p-2 text-gray-700 bg-white  border-l-2  transition  cursor-pointer uppercase font-medium flex justify-center hover:bg-yellow-600 focus:bg-yellow-600 focus:text-white focus:outline-none hover:text-white hover:ease-in duration-500 ...">Honey</Tab>
+                <Tab className="flex-grow p-2 text-gray-700 bg-white  border-l-2  transition  cursor-pointer uppercase font-medium flex justify-center hover:bg-yellow-600 focus:bg-yellow-600 focus:text-white focus:outline-none hover:text-white hover:ease-in duration-500 ...">Oils</Tab>
+                <Tab className="flex-grow p-2 text-gray-700 bg-white  border-l-2  transition  cursor-pointer uppercase font-medium flex justify-center hover:bg-yellow-600 focus:bg-yellow-600 focus:text-white focus:outline-none hover:text-white hover:ease-in duration-500 ...">Spice</Tab>
+                </div>
+            </TabList>
+            <TabPanel></TabPanel>
+            <TabPanel></TabPanel>
+            <TabPanel></TabPanel>
+            <TabPanel></TabPanel>
+            <TabPanel></TabPanel>
+            <TabPanel></TabPanel>
+        </Tabs>
 
     );
 };
 
 export default AllProduct;
 
-{/* <div className="container mx-auto mt-6">
-            <div className="flex bg-teal-100 rounded-md shadow-md">
-                <label className="flex-grow p-2 text-gray-700 bg-white rounded-l-md transition  cursor-pointer hover:bg-cyan-600 focus:bg-cyan-600 focus:text-white focus:outline-none hover:text-white hover:ease-in duration-500 ...">
-                    <input type="radio" name="radio" className="hidden" />
-                    <span className="text-sm uppercase text-center items-center justify-center flex font-medium">Dates</span>
-                </label>
 
-                <label className="flex-grow p-2 text-gray-700 bg-white transition  cursor-pointer hover:bg-cyan-600 focus:bg-cyan-600 focus:text-white focus:outline-none hover:text-white hover:ease-in duration-500 ...">
-                    <input type="radio" name="radio" className="hidden" />
-                    <span className="text-sm uppercase text-center items-center justify-center flex font-medium">Peanuts</span>
-                </label>
-
-                <label className="flex-grow p-2 text-gray-700 bg-white rounded-r-md transition  cursor-pointer hover:bg-cyan-600 focus:bg-cyan-600 focus:text-white focus:outline-none hover:text-white hover:ease-in duration-500 ...">
-                    <input type="radio" name="radio" className="hidden" />
-                    <span className="text-sm uppercase  items-center justify-center flex text-center font-medium">Seeds</span>
-                </label>
-
-                <label className="flex-grow p-2 text-gray-700 bg-white rounded-r-md transition  cursor-pointer hover:bg-cyan-600 focus:bg-cyan-600 focus:text-white focus:outline-none hover:text-white hover:ease-in duration-500 ...">
-                    <input type="radio" name="radio" className="hidden" />
-                    <span className="text-sm uppercase  items-center justify-center flex text-center font-medium">Honey</span>
-                </label>
-
-                <label className="flex-grow p-2 text-gray-700 bg-white rounded-r-md transition  cursor-pointer hover:bg-cyan-600 focus:bg-cyan-600 focus:text-white focus:outline-none hover:text-white hover:ease-in duration-500 ...">
-                    <input type="radio" name="radio" className="hidden" />
-                    <span className="text-sm uppercase  items-center justify-center flex text-center font-medium">Oils</span>
-                </label>
-
-                <label className="flex-grow p-2 text-gray-700 bg-white rounded-r-md transition  cursor-pointer hover:bg-cyan-600 focus:bg-cyan-600 focus:text-white focus:outline-none hover:text-white  hover:ease-in duration-500 ...">
-                    <input type="radio" name="radio" className="hidden" />
-                    <span className="text-sm uppercase  items-center justify-center flex text-center font-medium">Spice & Herbs</span>
-                </label>
-                
-            </div>
-        </div> */}
 
 
 
